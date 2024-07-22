41be5851352d6b7d61903cf3a2ecf7c88f34eaaa
elias-cannesson <ecanness@oberlin.edu>
2024-07-22 12:49:26 -0700

    including new repo arn in global variables + deploying new ResumeWebsite-reactPipeline

--- lib/global_variables.ts
+++ lib/global_variables.ts
@@ -5,3 +5,5 @@ export const website_cert_arn = "arn:aws:acm:us-east-1:654654597644:certificate/
 export const website_bucket_arn = "arn:aws:s3:::ecannesson.com-deployment";
 
 export const hosted_zone_id = "Z03704531OX9ZC1LKTCWX";
+
+export const react_repo_arn = "arn:aws:codecommit:us-west-2:654654597644:ResumeWebsiteGit";
---
+++ lib/react-pipeline.ts
@@ -0,0 +1,76 @@
+import { Stack, StackProps, RemovalPolicy, DefaultStackSynthesizer } from "aws-cdk-lib"
+import { Construct } from "constructs"
+import { BuildSpec, ComputeType, LinuxBuildImage, PipelineProject } from 'aws-cdk-lib/aws-codebuild';
+import { Repository } from 'aws-cdk-lib/aws-codecommit';
+import { Artifact, Pipeline } from 'aws-cdk-lib/aws-codepipeline';
+import { CodeBuildAction, CodeCommitSourceAction, S3DeployAction } from 'aws-cdk-lib/aws-codepipeline-actions';
+import { Bucket } from 'aws-cdk-lib/aws-s3';
+
+import { website_domain, website_bucket_arn, react_repo_arn } from './global_variables';
+
+export class ReactPipelineStack extends Stack {
+    constructor(scope: Construct, id: string, stage_Name: string, props?: StackProps) {
+        super(scope, id, {
+            synthesizer: new DefaultStackSynthesizer(), // Explicitly use the DefaultStackSynthesizer
+            ...props,
+          });
+
+        const websiteBucket = Bucket.fromBucketArn(this, 'websiteBucket', website_bucket_arn);
+
+        const reactBuildProject = new PipelineProject(this, 'ResumeWebsiteReactBuild', {
+            buildSpec: BuildSpec.fromSourceFilename('buildspec.yml'),
+            environment: {
+                buildImage: LinuxBuildImage.STANDARD_5_0,
+                computeType: ComputeType.SMALL
+            }
+        });
+
+        const artifactBucket = new Bucket(this, 'ResumeReactPipelineArtifactBucket', {
+            bucketName: 'resume-react-pipeline-artifact-bucket',
+            removalPolicy: RemovalPolicy.DESTROY
+        })
+        
+        const gitOutput = new Artifact('ResumeWebsiteReactBuildLatestMaster')
+        
+        const buildOutput = new Artifact('ResumeWebsiteReactBuildOutput')
+
+        new Pipeline(this, 'reactPipeline', {
+            artifactBucket,
+            pipelineName: 'ResumeWebsite-React-Pipeline',
+            stages: [
+                {
+                    stageName: 'source',
+                    actions: [
+                        new CodeCommitSourceAction({
+                            actionName: 'readLatestMasterCommit',
+                            output: gitOutput,
+                            repository: Repository.fromRepositoryArn(this, 'reactGitRepo', react_repo_arn)
+                        })
+                    ]
+                },
+                {
+                    stageName: 'build',
+                    actions: [
+                        new CodeBuildAction({
+                            actionName: 'buildReactApp',
+                            input: gitOutput,
+                            outputs: [buildOutput],
+                            project: reactBuildProject
+                        })
+                    ]
+                },
+                {
+                    stageName: 'deploy',
+                    actions: [
+                        new S3DeployAction({
+                            actionName: 'DeployReactApp',
+                            input: buildOutput,
+                            bucket: websiteBucket
+                        })
+                    ]
+                }
+            ]
+        });
+
+    }
+}
