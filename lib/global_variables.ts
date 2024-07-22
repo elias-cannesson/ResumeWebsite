37b7d1e8d296d8cefe489d21ff23c372ce7dc06c
elias-cannesson <ecanness@oberlin.edu>
2024-07-22 15:50:32 -0700

    changing arn for website bucket too

--- lib/global_variables.ts
+++ lib/global_variables.ts
@@ -2,7 +2,7 @@ export const website_domain = "ecannesson.com";
 
 export const website_cert_arn = "arn:aws:acm:us-east-1:654654597644:certificate/1f1333ee-b443-4a2c-8eab-950083412224";
 
-export const website_bucket_arn = "arn:aws:s3:::ecannesson.com";
+export const website_bucket_arn = "arn:aws:s3:::www.ecannesson.com";
 
 export const hosted_zone_id = "Z03704531OX9ZC1LKTCWX";
 
