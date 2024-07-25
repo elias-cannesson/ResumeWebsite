15814977b216a0338d8dfe431e10d2bfa2541b49
elias-cannesson <ecanness@oberlin.edu>
2024-07-25 15:14:47 -0700

    chaing hosted_zone_id to the id of the Public hosted zone: wanna see if this is what is causing https timeout

--- lib/global_variables.ts
+++ lib/global_variables.ts
@@ -4,6 +4,6 @@ export const website_cert_arn = "arn:aws:acm:us-east-1:654654597644:certificate/
 
 export const website_bucket_arn = "arn:aws:s3:::www.ecannesson.com";
 
-export const hosted_zone_id = "Z03704531OX9ZC1LKTCWX";
+export const hosted_zone_id = "Z02920461595SR9606W7W";
 
 export const react_repo_arn = "arn:aws:codecommit:us-west-2:654654597644:resume-website-git-repo";
