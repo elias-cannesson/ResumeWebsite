d42269aa28fb9fd65930e5e31ae793f1262fda7e
elias-cannesson <ecanness@oberlin.edu>
2024-07-29 18:50:43 -0700

    new arn for certificate

--- lib/global_variables.ts
+++ lib/global_variables.ts
@@ -1,6 +1,6 @@
 export const website_domain = "ecannesson.com";
 
-export const website_cert_arn = "arn:aws:acm:us-east-1:654654597644:certificate/84c47e69-c8f0-4020-999c-a4270982fee9";
+export const website_cert_arn = "arn:aws:acm:us-east-1:654654597644:certificate/1822b68a-2e15-4f23-a95e-23d6edfb92e4";
 
 export const website_bucket_arn = "arn:aws:s3:::www.ecannesson.com";
 
